fc6e5ba23d943d9d6c293f349fe5bebe19abed94
Astraea Bombita <abombita98@hotmail.com>
2021-07-02 14:41:42 -0400

    added index file

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use (express.static('public'));
+app.use(express.static('public'));
 
+/*
 app.get('/', function(req, res){
     console.log("my will to live");
 });
+*/
 
 
-app.listen(3001)
+app.listen(3050);
